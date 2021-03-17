961cfd962826080e94ac17ee05bcb51051395ed3
liaolivieri <80793479+liaolivieri@users.noreply.github.com>
2021-03-17 12:57:00 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -1,12 +1,24 @@
 # My first code in R for remote sensing!
-# Il mio primo codide in R per il telerilevamento!!!
+# Il mio primo codice in R per il telerilevamento!
 
-install.packages("raster")
+# Pacchetti da installare:
+# install.packages("raster")
+
+# Le librerie mi permettono di aprire i pacchetti appena intallati:
 library(raster)
+
 # Setwd mi permette di creare un collegamento tra la cartella lab e R. 
 setwd("C:/lab/") # Windows
-# Brick mi permette di importare un immagine satellitare contenuta nel mio pc.
+# Brick mi permette di importare un immagine satellitare contenuta nel mio pc nella cartella lab.
 p224r63_2011 <- brick("p224r63_2011_masked.grd")
 p224r63_2011
 # Plot mi permette di visualizzare l'immagine appena caricata.
 plot(p224r63_2011)
+#
+# ColorRampPalette permette di cambiare il colore
+cl <- colorRampPalette(c("black","grey","light grey")) (100)
+plot(p224r63_2011, col=cl)
+ 
+# Cambiamo colori a nostro piacimento
+cls <- colorRampPalette(c("blue","purple","red","orange","yellow","green")) (100)
+plot(p224r63_2011, col=cls)
