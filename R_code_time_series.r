190914377b3ac3bff94c9c27050416bd4b3aa720
liaolivieri <80793479+liaolivieri@users.noreply.github.com>
2021-04-07 13:04:58 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -22,16 +22,16 @@ lst_2010 <- raster("lst_2010.tif")
 plot(lst_2010)
 lst_2015 <- raster("lst_2015.tif")
 plot(lst_2015)
-
-#Fare un pannello con le 4 immagini:
+#Facciamo un pannello con le 4 immagini:
 par(mfrow=c(2,2))
 plot(lst_2000)
 plot(lst_2005)
 plot(lst_2010)
 plot(lst_2015)
+
 #Importiamo le immagini tutte insieme con la funzione: lapply
-#Prima facciamo la lista dei nostri file per poter usare la funzione lapply
-#con il pattern identifichiamo la scritta in comune a tutti i file quindi nel nostro caso lst
+#Prima facciamo la lista dei nostri file per poter usare questa funzione
+#Con il pattern identifichiamo la scritta in comune a tutti i file quindi nel nostro caso lst
 rlist <- list.files(pattern="lst")
 rlist
 #Ora che abbiamo la nostra lista con i nostri file applichiamo lapply a tutta la lista associata alla funzione raster
@@ -41,9 +41,14 @@ import
 TGr <- stack(import)
 #Quindi adesso possiamo plottare direttamente il pacchetto TGr
 plot(TGr)
-
-
-
+#Possiamo plottarle tutte assime con RGB
+#1=mappa 2000, 2=mappa 2005, 3=mappa 2010, 4=mappa 2015
+#Mappa blu del 2010 (perchè B=3) 
+plotRGB(TGr, 1, 2, 3, stretch="Lin")
+#Mappa blu del 2015(perchè B=4)
+plotRGB(TGr, 2, 3, 4, stretch="Lin")
+#invertiamo i valori ma la mappa non cambia di molto
+plotRGB(TGr, 4, 3, 2, stretch="Lin")
 
 
 
