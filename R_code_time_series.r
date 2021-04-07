99870b2e369885cf55f690a59bf1cc2fd2b27fd6
liaolivieri <80793479+liaolivieri@users.noreply.github.com>
2021-04-07 12:34:41 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -2,10 +2,26 @@
 #Greenland increase of temperature
 #Data and code from Emanuela Cosma
 
+#Pacchetti da installare
 #install.packages("raster")
 #install.packages("rasterVis")
 
+#Librerie che ci permettono di usare i nostri pacchetti installati:
 library(raster)
 library(rasterVis)
 
 setwd("C:/lab/greenland") # Windows
+
+#La funzione per caricaricare singoli dati è raster (del pacchetto raster)
+#Carichiamo tutti le immagini lst della groenlandia del 2000, 2005, 2010, 2015
+lst_2000 <- raster("lst_2000.tif")
+plot(lst_2000)
+lst_2005 <- raster("lst_2005.tif")
+plot(lst_2005)
+lst_2010 <- raster("lst_2010.tif")
+plot(lst_2010)
+lst_2015 <- raster("lst_2015.tif")
+plot(lst_2015)
+
+#fare un pannello con le 4 immagini:
+par(mfrow=c(2,2))
