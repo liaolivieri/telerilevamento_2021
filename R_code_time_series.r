b9220373bac1294d9214bb970717625a0fe70521
liaolivieri <80793479+liaolivieri@users.noreply.github.com>
2021-04-09 11:00:29 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -51,6 +51,45 @@ plotRGB(TGr, 2, 3, 4, stretch="Lin")
 plotRGB(TGr, 4, 3, 2, stretch="Lin")
 
 
+### Day 2
+#Ricarichiamo il codice dell'altra volta 
+rlist <- list.files(pattern="lst")
+rlist 
+import <- lapply(rlist,raster)
+import 
+TGr <- stack(import)
+TGr
+#Usiamo la funzione levelplot che permette di plottare la nosta immagine in modo più bello graficamente con una solo scala e una sola leggenda 
+levelplot(TGr)
+#legato ad una solo mappa con statistiche laterali 
+levelplot(TGr$lst_2000)
+#Abbelliamo il levelplot quindi possiamo usare la colorRampPalette perchè usiamo immagini singole (TGr)
+#Il blu scuro temperatura più bassa a red temperature più alte
+cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
+levelplot(TGr,  col.regions=cl)
+#Possiamo cambiare i titoli di queste immagini definiti attributi 
+levelplot(TGr,col.regions=cl, names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+#Diamo anche un titolo (main)
+levelplot(TGr,col.regions=cl, main="LST variation in time", names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
 
-
+#Usiamo i dati Melt
+#Sono tantisimi dati quindi faremo di nuovo una lista
+meltlist <- list.files(pattern="melt")
+meltlist
+#Usiamo la funzione lapply
+melt_import <- lapply(meltlist,raster)
+melt_import 
+#Facciamo lo stack 
+melt <- stack(melt_import)
+melt
+#Facciamo il levelplot
+levelplot(melt)
+#Matrixalgebra cioè l'algebra associata alle matrici andiamo a vedere il valore di scioglimento nel tempo 
+#sottrazione tra gli strati del 2007 e del 1979
+melt_amount <- melt$X2007annual_melt - melt$X1979annual_melt
+#ColorRampPalette le zone rosse sono quelle che dal 1979 al 2009 c'è stato uno scioglimento del ghiacciopiù alto
+clb <- colorRampPalette(c("blue","white","red"))(100)
+plot(melt_amount, col=clb)
+#Facciamo un level plot di melt_ammonut
+levelplot(melt_amount, col.regions=clb)
 
