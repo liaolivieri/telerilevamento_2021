07519738f6bc896b02cf00f2301600f564b0d989
liaolivieri <80793479+liaolivieri@users.noreply.github.com>
2021-04-16 09:32:49 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -1,7 +1,12 @@
 # R_code_knitr.r
 
+#Il pacchetto knitr può usare un codice esterno per creare un report
+#Si prende il codice all'esterno e lo importa, e genererà un report è verrà salvato nella cartella in cui è salvato il codcie
+
+#Pacchetti da istallare:
 #install.packages("knitr")
 
+#Librerie:
 library(knitr)
 
 setwd("C:/lab/") # Windows
