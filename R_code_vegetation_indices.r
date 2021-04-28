ce158bf970874f78feb974c899e9d53fe2a4d669
liaolivieri <80793479+liaolivieri@users.noreply.github.com>
2021-04-28 12:58:25 +0200

    Create R_code_vegetation_indices.r

---
+++ R_code_vegetation_indices.r
@@ -0,0 +1,15 @@
+#R_code_vegetation_indices.r
+
+library(raster)
+
+setwd("C:/lab/") #Windows
+
+#Carichiamo le immagini 
+defor1 <- brick("defor1.jpg")
+defor2 <- brick("defor2.jpg")
+
+#Plottiamo le immagini 
+#b1=NIR, b2=red, b3=green
+par(mfrow=c(2,1))
+plotRGB(defor1, r=1, g=2, b=3, stretch="lin")
+plotRGB(defor2, r=1, g=2, b=3, stretch="lin")
