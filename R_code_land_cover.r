3314719a3c05fea16ab65273034eb9b9a02025ab
liaolivieri <80793479+liaolivieri@users.noreply.github.com>
2021-05-07 10:50:21 +0200

    Update R_code_land_cover.r

--- R_code_land_cover.r
+++ R_code_land_cover.r
@@ -39,8 +39,65 @@ p2 <- ggRGB(defor2, r=1, g=2, b=3, stretch="lin")
 grid.arrange(p1, p2, nrow=2)
 
 
+### Day 2
 
+#Dividiamo in 2 classi le nostre immagini (1 agricola, 1 foresta)
+#Unsupervised classification
+d1c <- unsuperClass(defor1, nClasses= 2)
+d1c
+plot(d1c$map)
+#Class1: foresta
+#Class2: agricoltura
+#Con la funzione set.seed() si possono avere gli stessi risultati tutte le volte
+d2c <- unsuperClass(defor2, nClasses= 2)
+d2c
+plot(d2c$map)
+#Class1: foresta
+#Class2: agricoltura
+#Facciamo di defor2 3 classi 
+d2c3 <- unsuperClass(defor2, nClasses= 3)
+plot(d2c3$map)
+#Class1: foresta
+#Class2: agricoltura
+#Andiamo a vedere quanta frequenza c'è delle due classi create
+freq(d1c$map)
+#class1= 304618
+#class2= 36674
+#Facciamo la somma
+s1 <- 304618 + 36674
+#s1= 341292
+#Calcoliamo la proporzione tra le due classi
+prop1 <- freq(d1c$map) / s1
+prop1
+#prop foresta: 0.8925436 ,
+#prop agricoltura: 0.1074564
+#Facciamo lo stesso per la seconda mappa
+s2 <- 342726
+prop2 <- freq(d2c$map) / s2
+prop2
+#prop foresta: 0.5197388
+#prop agricoltura: 0.4802612
 
+#Facciamo un dataset 
+#cover=Prima colonna fattori (valori categoriche: foresta, agricoltura)
+#percent_1992= seconda colonna(valori percetuale 1992)
+#percent_2006= terza colonna(valori percentuale 2006)
+cover <- c("Forest", "Agriculture")
+percent_1992 <- c(89.25, 10.75)
+percent_2006 <- c(51.97, 48.03)
 
+percentages <- data.frame(cover, percent_1992, percent_2006)
+percentages
+#        cover percent_1992 percent_2006
+# 1      Forest        89.25        51.97
+# 2 Agriculture        10.75        48.03
 
+#Facciamo un grafico con ggplot
+#aes=estetica, geom_bar= grafico a barre
+p1 <- ggplot(percentages, aes(x=cover, y=percent_1992, color=cover)) + geom_bar(stat="identity", fill="white")
+p1
+p2 <- ggplot(percentages, aes(x=cover, y=percent_2006, color=cover)) + geom_bar(stat="identity", fill="white")
+p2
 
+#Mettiamo assime i nostri due grafici
+grid.arrange(p1, p2, nrow=1)
