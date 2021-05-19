7169bff3a180237607ba24798c23fefff883cf80
liaolivieri <80793479+liaolivieri@users.noreply.github.com>
2021-05-19 12:50:00 +0200

    Update R_code_variability.r

--- R_code_variability.r
+++ R_code_variability.r
@@ -32,7 +32,7 @@ plot(ndvi,col=cl)
 
 #Calcoliamo la variabilità dell'immagine con la funzione focal 
 #Deviazione standard in R si dice sd
-#w=windows della matrice di ogni pixel su 9, su 3 righe e 3 colonne 
+#w=windows della matrice di ogni pixel su 9 totali, disposti su 3 righe per 3 colonne 
 #Meglio calcolare finestre quadrate, più pixel più sarà lungo il calcolo
 ndvisd3 <- focal(ndvi, w=matrix(1/9, nrow=3, ncol=3), fun=sd)
 plot(ndvisd3)
@@ -50,12 +50,28 @@ plot(ndvimean3, col=clsd)
 
 #Cambiamo la grandezza della nostra moving windows (deve essere sempre dispari)
 #Usiamo 13*13 pixel 
-ndvisd13 <- focal(ndvi, w=matrix(1/169, nrow=3, ncol=3), fun=sd)
+ndvisd13 <- focal(ndvi, w=matrix(1/169, nrow=13, ncol=13), fun=sd)
 plot(ndvisd13, col=clsd)
 
+#cambiano ancora la gradezza
+#usiamo 5*5 pixel
+ndvisd5 <- focal(ndvi, w=matrix(1/25, nrow=5, ncol=5), fun=sd)
+plot(ndvisd5, col=clsd)
 
 
-
+#Possiamo usare un'altra tecnica la PCA ed usiamo solo la prima componente principale con la funzione rasterPCA
+#PCA di sent
+sentpca <- rasterPCA(sent)
+plot(sentpca$map)
+#PC1 è la migliore, man mano che passiamo alle altre le informazioni diminuisono drasticamente
+sentpca
+#Vediamo quanta proporzione di varianza viene spiegata in ogni componente
+summary(sentpca$model)
+#                          Comp.1     Comp.2      Comp.3 Comp.4
+# Standard deviation     77.3362848 53.5145531 5.765599616      0
+# Proportion of Variance  0.6736804  0.3225753 0.003744348      0
+# Cumulative Proportion   0.6736804  0.9962557 1.000000000      1
+#la prima PC continente il 67,36804% della informazione originale
 
 
 
