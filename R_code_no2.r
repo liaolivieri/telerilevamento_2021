3301dad6d46056a4131f118263e87dcc0445bbe9
liaolivieri <80793479+liaolivieri@users.noreply.github.com>
2021-08-03 10:34:12 +0200

    Create R_code_no2.r

---
+++ R_code_no2.r
@@ -0,0 +1,23 @@
+# R_code_no2.r
+
+library(raster)
+
+
+# 1. Set the working directory EN
+
+setwd("C:/lab/EN") #Windwos
+
+# 2. Import the first image (single band)
+# we will select band 1, but the raster funciotion enables to select other single-bands
+
+EN01 <- raster("EN_0001.png")
+
+
+
+
+
+
+
+
+
+
