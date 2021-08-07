42bf42dff1edbcfdb2acca0f92f9ee9865f52ef0
liaolivieri <80793479+liaolivieri@users.noreply.github.com>
2021-08-07 19:33:25 +0200

    Update R_exam_project.r

--- R_exam_project.r
+++ R_exam_project.r
@@ -1,6 +1,6 @@
 # R_exam_project.r
 
-#Drought in California
+#Drought in California in the Lake Shasta from 2019 to 2021
 
 #Library:
 library(raster)
@@ -12,58 +12,64 @@ library(rasterVis)
 
 setwd("C:/lab/exam") #Windows
 
-# Uploud 4 images from Earth of Observatory
+# Uploud 2 images from Earth of Observatory 
 # Lake Shasta
-shasta2019 <- raster("shasta_oli_2019194_lrg.jpg")
+shasta2019 <- brick("shasta_oli_2019194_lrg.jpg")
 plot(shasta2019)
-shasta2021 <- raster("shasta_oli_2021167_lrg.jpg")
+shasta2021 <- brcik("shasta_oli_2021167_lrg.jpg")
 plot(shasta2021)
 
-# Lake Oroville
-oro2019 <- raster("oroville_oli_2019155_lrg.jpg")
-plot(oro2019)
-oro2021 <- raster("oroville_oli_2021160_lrg.jpg")
-plot(oro2021)
+#Plot the 3 band with the function plotRGB
+par(mfrow=c(2,2))
+plotRGB(shasta2019,r=1,g=2,b=3, stretch="Hist")
+plotRGB(shasta2021,r=1,g=2,b=3, stretch="Hist")
+
+# Upload with function raster
+shasta2019r <- raster("shasta_oli_2019194_lrg.jpg")
+shasta2021r <- raster("shasta_oli_2021167_lrg.jpg")
 
 # Choose the color 
 cl <- colorRampPalette(c("blue","light blue","yellow","orange","pink","red"))(200)
 
-# Create 2 stack of the 2 lake
-lshasta <- stack(shasta2019, shasta2021)
-loroville <- stack(oro2019, oro2021)
+# Create a stack of the Lake
+lshasta <- stack(shasta2019r, shasta2021r)
 
-#Plot the 2 stack
+#Plot the stack with levelplot
 levelplot(lshasta,col.regions=cl, main="Lake Shasta", names.attr=c("July 2019","June 2021"))
-levelplot(loroville,col.regions=cl, main="Lake Oroville", names.attr=c("June 2019","June 2021"))
 
-# Paris
-pairs(lshasta)
-pairs(lroville)
+
+#............................................................................................................................................................
 
 # MULTIVARIATE ANALYSIS
 
-# PCA of Lake Shasta
-lshasta_pca <- rasterPCA(lshasta)
-summary(lshasta_pca$model)
-#Importance of components:
-#                            Comp.1     Comp.2
-# Standard deviation     82.5248997 36.7753106
-# Proportion of Variance  0.8343183  0.1656817
-# Cumulative Proportion   0.8343183  1.0000000
-levelplot(lshasta_pca$map,col.regions=cl, main="PCA Lake Shasta", names.attr=c("July 2019","June 2021"))
-
-
-# PCA of Lake Oroville
-loroville_pca <- rasterPCA(loroville)
-summary(loroville_pca$model)
+# PCA of Lake Shasta 2019
+shasta2019_pca <- rasterPCA(shasta2019)
+summary(shasta2019_pca$model)
+#Results
 # Importance of components:
-#                            Comp.1     Comp.2
-# Standard deviation     89.9813719 57.9840457
-# Proportion of Variance  0.7065879  0.2934121
-# Cumulative Proportion   0.7065879  1.0000000
-levelplot(loroville_pca$map,col.regions=cl, main="PCA Lake Oroville", names.attr=c("June 2019","June 2021"))
+#                            Comp.1      Comp.2      Comp.3
+# Standard deviation     72.8708129 15.49184619 6.569193764
+# Proportion of Variance  0.9493767  0.04290794 0.007715347
+# Cumulative Proportion   0.9493767  0.99228465 1.000000000
+plotRGB(shasta2019_pca$map,r=1,g=2,b=3, stretch="Hist")
 
-#
+# PCA of Lake Shasta 2021
+shasta2021_pca <- rasterPCA(shasta2021)
+summary(shasta2021_pca$model)
+#Results
+# Importance of components:
+#                           Comp.1      Comp.2      Comp.3
+# Standard deviation     95.682841 20.37762526 7.315502912
+# Proportion of Variance  0.951292  0.04314722 0.005560759
+# Cumulative Proportion   0.951292  0.99443924 1.000000000
+plotRGB(shasta2019_pca$map,r=1,g=2,b=3, stretch="Hist")
+
+# Comparison PCA 2019 and PCA 2021 
+par(mfrow=c(1,2))
+plotRGB(shasta2019_pca$map,r=1,g=2,b=3, stretch="Hist")
+plotRGB(shasta2021_pca$map,r=1,g=2,b=3, stretch="Hist")
+
+
 
 
 
