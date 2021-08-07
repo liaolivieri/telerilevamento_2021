b3d985eef75620d29f5ad059b62fb915fe03bb21
liaolivieri <80793479+liaolivieri@users.noreply.github.com>
2021-08-07 21:48:57 +0200

    Update R_exam_project.r

--- R_exam_project.r
+++ R_exam_project.r
@@ -15,7 +15,7 @@ setwd("C:/lab/exam") #Windows
 # Lake Shasta
 shasta2019 <- brick("shasta_oli_2019194_lrg.jpg")
 plot(shasta2019)
-shasta2021 <- brcik("shasta_oli_2021167_lrg.jpg")
+shasta2021 <- brick("shasta_oli_2021167_lrg.jpg")
 plot(shasta2021)
 
 #Plot the 3 band with the function plotRGB
@@ -28,7 +28,7 @@ shasta2019r <- raster("shasta_oli_2019194_lrg.jpg")
 shasta2021r <- raster("shasta_oli_2021167_lrg.jpg")
 
 # Choose the color 
-cl <- colorRampPalette(c("blue","light blue","yellow","orange","pink","red"))(200)
+cl <- colorRampPalette(c("blue","light blue","yellow","orange","pink","red"))(100)
 
 # Create a stack of the Lake
 lshasta <- stack(shasta2019r, shasta2021r)
@@ -40,11 +40,12 @@ levelplot(lshasta,col.regions=cl, main="Lake Shasta", names.attr=c("July 2019","
 #............................................................................................................................................................
 
 # MULTIVARIATE ANALYSIS
+# 1. Principal component analysis
 
-# PCA of Lake Shasta 2019
+# PCA of Lake Shasta 2019 
 shasta2019_pca <- rasterPCA(shasta2019)
 summary(shasta2019_pca$model)
-#Results
+#Results:
 # Importance of components:
 #                            Comp.1      Comp.2      Comp.3
 # Standard deviation     72.8708129 15.49184619 6.569193764
@@ -55,7 +56,7 @@ plotRGB(shasta2019_pca$map,r=1,g=2,b=3, stretch="Hist")
 # PCA of Lake Shasta 2021
 shasta2021_pca <- rasterPCA(shasta2021)
 summary(shasta2021_pca$model)
-#Results
+#Results:
 # Importance of components:
 #                           Comp.1      Comp.2      Comp.3
 # Standard deviation     95.682841 20.37762526 7.315502912
@@ -73,9 +74,8 @@ sh19 <- ggRGB(shasta2019_pca$map,r=1,g=2,b=3, stretch="Hist")
 sh21 <- ggRGB(shasta2021_pca$map,r=1,g=2,b=3, stretch="Hist")
 grid.arrange(sh19, sh21, nrow=1)
 
-#.....................................................................................................................................................
 
-# Classification in 16 classes
+# 2. Classification in 16 classes
 
 unsh19 <- unsuperClass(shasta2019, nClasses=16)
 plot(unsh19$map)
@@ -87,7 +87,54 @@ par(mfrow=c(1,2))
 plot(unsh19$map)
 plot(unsh21$map)
 
+#......................................................................................................................................................
 
+# DVI - Difference Vegetation Index
+
+# See how the bands of NIR and RED are called 
+shasta2019
+#B1(NIR)=shasta_oli_2019194_lrg.1,; B2(RED)=shasta_oli_2019194_lrg.2
+shasta2021
+#B1(NIR)=shasta_oli_2021167_lrg.1; B2(RED)=shasta_oli_2021167_lrg.2
+
+# First index of Shasta 2019: NIR - RED
+dvi1 <- shasta2019$shasta_oli_2019194_lrg.1 - shasta2019$shasta_oli_2019194_lrg.2
+plot(dvi1)
+cld <- colorRampPalette(c('darkblue','yellow','red','black'))(100)
+plot(dvi1, col=cld, main="DVI of Shasta in 2019")
+
+# First index of Shasta 2021: NIR - RED
+dvi2 <- shasta2021$shasta_oli_2021167_lrg.1 - shasta2021$shasta_oli_2021167_lrg.2
+plot(dvi2)
+plot(dvi2, col=cld, main="DVI of Shasta in 2021")
+
+# Comparison togher for the difference
+par(mfrow=c(1,2))
+plot(dvi1, col=cld, main="DVI of Shasta in 2019")
+plot(dvi2, col=cld, main="DVI of Shasta in 2021")
+difdvi <- dvi1 - dvi2
+cldd <- colorRampPalette(c('blue','white','red'))(100)
+plot(difdvi, col=cldd)
+
+
+# NDVI - Normalized Difference Vegetation Index
+
+# NDVI= (NIR-RED) / (NIR+RED)
+# NDVI of Lake Shasta 2019
+ndvi1 <- (dvi1) / (shasta2019$shasta_oli_2019194_lrg.1 + shasta2019$shasta_oli_2019194_lrg.2)
+plot(ndvi1, col=cld, main="NDVI of Shasta in 2019")
+
+# NDVI of Lake Shasta 2021
+ndvi2 <- (dvi2) / (shasta2021$shasta_oli_2021167_lrg.1 + shasta2021$shasta_oli_2021167_lrg.2)
+plot(ndvi2, col=cld, main="NDVI of Shasta in 2021")
+
+# Difference NDVI
+difndvi <- ndvi1 - ndvi2
+plot(difndvi, col=cldd)
+
+
+#.....................................................................................................................................
+# SPECTRAL SIGNATURES
 
 
 
