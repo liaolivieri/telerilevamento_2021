1c0bebd6bf9a2d5c36d170b86efef3e81486344f
liaolivieri <80793479+liaolivieri@users.noreply.github.com>
2021-08-08 12:56:48 +0200

    Update R_exam_project.r

--- R_exam_project.r
+++ R_exam_project.r
@@ -1,6 +1,7 @@
 # R_exam_project.r
 
-#Drought in California in the Lake Shasta from 2019 to 2021
+# Drought in California in the Lake Shasta from 2019 to 2021
+# Images from: https://earthobservatory.nasa.gov/images/148447/california-reservoirs-reflect-deepening-drought
 
 #Library:
 library(raster)
@@ -8,6 +9,7 @@ library(rasterVis)
 library(RStoolbox)
 library(ggplot2)
 library(gridExtra)
+library(rgdal)
 
 setwd("C:/lab/exam") #Windows
 
@@ -40,7 +42,17 @@ levelplot(lshasta,col.regions=cl, main="Lake Shasta", names.attr=c("July 2019","
 #............................................................................................................................................................
 
 # MULTIVARIATE ANALYSIS
-# 1. Principal component analysis
+
+# 1. The function pairs produce a matrix of scatterplots.
+
+# Plots the correlations between the 3 bands of my stack.
+# The values of indices correlation range from 0 to 1: 1= correlation, 0= no correlation
+# 
+pairs(lshasta, main="Comparation with the function pairs")
+# Result= 0.65
+
+
+# 2. Principal component analysis
 
 # PCA of Lake Shasta 2019 
 shasta2019_pca <- rasterPCA(shasta2019)
@@ -75,44 +87,9 @@ sh21 <- ggRGB(shasta2021_pca$map,r=1,g=2,b=3, stretch="Hist")
 grid.arrange(sh19, sh21, nrow=1)
 
 
-# 2. Unsupervised classification
-
-# Multiframe with ggplot2 e gridExtra for the 2 images of 2019 and 2021
-l1 <- ggRGB(shasta2019, r=1, g=2, b=3, stretch="lin")
-l2 <- ggRGB(shasta2021, r=1, g=2, b=3, stretch="lin")
-grid.arrange(l1, l2, nrow=1)
-
-# classification in 2 classes: (1 forest, 1 water)
-l1c <- unsuperClass(shasta2019, nClasses= 2)
-l1c
-plot(l1c$map)
-# Class1: forest
-# Class2: water
-l2c <- unsuperClass(shasta2021, nClasses= 2)
-l2c
-plot(l2c$map)
-freq(l1c$map)
-#      value   count
-# forest[1,]     1  664187
-# water[2,]     2 2726390
-freq(l2c$map)
-#      value   count
-# forest[1,]     1 2705449
-# water[2,]     2  685128
-
-unsh19 <- unsuperClass(shasta2019, nClasses=16)
-plot(unsh19$map)
-
-unsh21 <- unsuperClass(shasta2021, nClasses=16)
-plot(unsh21$map)
-
-par(mfrow=c(1,2))
-plot(unsh19$map)
-plot(unsh21$map)
-
 #......................................................................................................................................................
 
-# DVI - Difference Vegetation Index
+# 1. DVI - Difference Vegetation Index
 
 # See how the bands of NIR and RED are called 
 shasta2019
@@ -140,7 +117,7 @@ cldd <- colorRampPalette(c('blue','white','red'))(100)
 plot(difdvi, col=cldd)
 
 
-# NDVI - Normalized Difference Vegetation Index
+# 2. NDVI - Normalized Difference Vegetation Index
 
 # NDVI= (NIR-RED) / (NIR+RED)
 # NDVI of Lake Shasta 2019
@@ -155,17 +132,72 @@ plot(ndvi2, col=cld, main="NDVI of Shasta in 2021")
 difndvi <- ndvi1 - ndvi2
 plot(difndvi, col=cldd)
 
-
 #.....................................................................................................................................
 # SPECTRAL SIGNATURES
 
+# Create a spectral signature of images Lake Shasta 2019 with the function "click"
+plotRGB(shasta2019, r=1, g=2, b=3, stretch="lin")
+click(shasta2019, id=T, xy=T, cell=T, type="p", pch=16, col="yellow")
+# In a pixel of vegetation -> B1= very high value, B2=low value, B3= average value
+# In a pixel of water -> B1= low value, B2= high value, B3= very high value
+# Results
+#        x      y    cell shasta_oli_2019194_lrg.1 shasta_oli_2019194_lrg.2 shasta_oli_2019194_lrg.3
+# 1  521.5 1201.5  966665                       45                      133                      119
+# 2 1685.5  749.5 1879513                        4                       53                       60
+# 3 1356.5 1289.5  790004                       32                      115                      105
+# 4 1557.5 1037.5 1298489                        9                       32                       40
+# 5  412.5  304.5 2775805                        2                       43                       65
+# 6  646.5  584.5 2211279                        4                       53                       67
 
 
+# Create a spectral signature of images Lake Shasta 2021  with the function "click"
+plotRGB(shasta2021, r=1, g=2, b=3, stretch="hist")
+click(shasta2021, id=T, xy=T, cell=T, type="p", pch=16, col="yellow")
+# Results:
+#        x      y    cell shasta_oli_2021167_lrg.1 shasta_oli_2021167_lrg.2 shasta_oli_2021167_lrg.3
+# 1  499.5  935.5 1503165                        4                       62                       76
+# 2 1674.5  745.5 1887570                      254                      249                      227
+# 3 1294.5 1227.5  914996                       26                       93                       84
+# 4 1550.5 1041.5 1290414                       10                       33                       39
+# 5  393.5  326.5 2731412                       37                      108                      130
+# 6  652.5  523.5 2334322                       57                      137                      148
 
+# Create a dataset with our results (just a few more representative results)
+band <- c(1,2,3)
+Shasta2019p1 <- c(45, 133, 119)
+Shasta2019p2 <- c(4, 53, 60)
+Shasta2019p5 <- c(2, 43, 65)
+Shasta2019p6 <- c(4, 53, 67)
+Shasta2021p1 <- c(4, 62, 76)
+Shasta2021p2 <- c(254, 249, 227)
+Shasta2021p5 <- c(37, 108, 130)
+Shasta2021p6 <- c(57, 137, 148)
+spectralst <- data.frame(band,Shasta2019p1,Shasta2019p2,Shasta2019p5,Shasta2019p6,Shasta2021p1,Shasta2021p2,Shasta2021p5,Shasta2021p6)
 
+# Plot this dataset with ggplot
+# Red for the results of 2019, blue for the results of 2021
+ggplot(spectralst, aes(x=band)) +
+geom_line(aes(y = Shasta2019p1), color="blue") +
+geom_line(aes(y = Shasta2019p2), color="blue") +
+geom_line(aes(y = Shasta2019p5), color="blue") +
+geom_line(aes(y = Shasta2019p6), color="blue") +
+geom_line(aes(y = Shasta2021p1), color="red") +
+geom_line(aes(y = Shasta2021p2), color="red") +
+geom_line(aes(y = Shasta2021p5), color="red") +
+geom_line(aes(y = Shasta2021p6), color="red") +
+labs(x="band", y="reflectance")
 
+# Plot this dataset with ggplot
+# The light color represent the results of 2019, the dark color represent the results of 2021
+ggplot(spectralst, aes(x=band)) +
+geom_line(aes(y = Shasta2019p1), color="light blue") +
+geom_line(aes(y = Shasta2019p2), color="pink") +
+geom_line(aes(y = Shasta2019p5), color="yellow") +
+geom_line(aes(y = Shasta2019p6), color="gray") +
+geom_line(aes(y = Shasta2021p1), color="blue") +
+geom_line(aes(y = Shasta2021p2), color="purple") +
+geom_line(aes(y = Shasta2021p5), color="orange") +
+geom_line(aes(y = Shasta2021p6), color="black") +
+labs(x="band", y="reflectance")
 
-
-
-
-
+#........................................................................................................................................
